1b84adb32c13473f802d47a9c939c3819d7cb0ed
JamesAskelson <jamesaskelson630@gmail.com>
2024-09-26 22:33:19 -0500

    simple-angular-style-learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
+import { NgClass, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgStyle, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ng-styles-learning';
+  loggedin = false;
 }
